a039566b3d8467fa338381b565e59fce13061b80
Vignesh Raja <vignesh@optimizely.com>
2018-08-19 23:50:58 -0700

    Modify tests for new HLL struct definition

--- hll.go
+++ hll.go
@@ -1,23 +1,46 @@
 package hll
 
+import (
+  "github.com/spaolacci/murmur3"
+  "hash"
+  "math"
+  "math/bits"
+  // "fmt"
+)
+
 type HLL struct {
+  numRegisterBits int
   registers []int
+  murmur32 hash.Hash32
 }
 
-func NewHLL(numRegisters int) HLL {
+func NewHLL(numRegisterBits int) HLL {
+  numRegisters := int(math.Exp2(float64(numRegisterBits)))
   registers := make([]int, numRegisters)
-  hllInstance := HLL {registers}
+  murmur32 := murmur3.New32()
+  hllInstance := HLL {numRegisterBits, registers, murmur32}
   return hllInstance
 }
 
-func add() {
+func (hll HLL) Add(value string) {
+  hll.murmur32.Write([]byte(value))
+  hashedValue := hll.murmur32.Sum32()
+  // bit mask to fetch bits representing register index to update
+  maskRegisterBits := ^uint32(0) >> uint32(32 - hll.numRegisterBits)
+  registerIndex := uint32(hashedValue & maskRegisterBits)
+  remainingBits := uint32(hashedValue) >> uint32(hll.numRegisterBits)
+  trailingZeroes := bits.TrailingZeros32(remainingBits)
+  registerValue := 0
+  if trailingZeroes != 32 {
+    registerValue = trailingZeroes
+  }
+  hll.registers[registerIndex] = int(math.Max(float64(hll.registers[registerIndex]), float64(registerValue)))
+}
+
+func Count() {
 
 }
 
-func count() {
-
-}
-
-func merge() {
+func Merge() {
 
 }
--- hll_test.go
+++ hll_test.go
@@ -6,11 +6,16 @@ import (
 )
 
 func TestNewHLL(t *testing.T) {
-  numRegisters := 5
-  hllInstance := NewHLL(numRegisters)
+  numRegisterBits := 6
+  numRegisters := 64
+  hllInstance := NewHLL(numRegisterBits)
+  assert.Equal(t, numRegisterBits, hllInstance.numRegisterBits, "Num register bits should be equal")
   expectedRegisters := make([]int, numRegisters)
   assert.Equal(t, expectedRegisters, hllInstance.registers, "Registers should be equal")
 }
 
 func TestAdd(t *testing.T) {
+  numRegisterBits := 6
+  hllInstance := NewHLL(numRegisterBits)
+  hllInstance.Add("hello")
 }

a7c633128007eb9424c7228a40f52b36a725d09e
Vignesh Raja <vignesh@optimizely.com>
2018-08-24 23:44:28 -0700

    Add boilerplate for Count test

--- hll_test.go
+++ hll_test.go
@@ -5,6 +5,7 @@ import (
   "github.com/stretchr/testify/assert"
   "github.com/golang/mock/gomock"
   "go-hll/mocks"
+  "fmt"
 )
 
 func TestNewHLLWithRegisterBits(t *testing.T) {
@@ -36,4 +37,20 @@ func TestAdd(t *testing.T) {
       assert.Equal(t, 0, registerVal)
     }
   }
+}
+
+func TestCount(t *testing.T) {
+  numRegisterBits := 6
+  numRegisters := 64
+  registers := make([]int, numRegisters)
+  mockCtrl := gomock.NewController(t)
+  defer mockCtrl.Finish()
+  mockMurmur32 := mocks.NewMockHash32(mockCtrl)
+  hllInstance := HLL {numRegisterBits, registers, mockMurmur32}
+  mockMurmur32.EXPECT().Write([]byte("hello"))
+  // 111000001010011100110110
+  mockMurmur32.EXPECT().Sum32().Return(uint32(2903491477))
+  hllInstance.Add("hello")
+  fmt.Printf("%d\n", hllInstance.registers[21])
+  fmt.Printf("%f\n", hllInstance.Count())
 }

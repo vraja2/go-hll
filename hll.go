f754c17305ba781c99d3120ac451c651bb2d617a
Vignesh Raja <vignesh@optimizely.com>
2018-08-25 12:11:38 -0700

    Run gofmt

--- hll.go
+++ hll.go
@@ -1,111 +1,111 @@
 package hll
 
 import (
-  "errors"
-  "github.com/spaolacci/murmur3"
-  "hash"
-  "math"
-  "math/bits"
+	"errors"
+	"github.com/spaolacci/murmur3"
+	"hash"
+	"math"
+	"math/bits"
 )
 
 type HLL struct {
-  numRegisterBits int
-  registers []int
-  murmur32 hash.Hash32
+	numRegisterBits int
+	registers       []int
+	murmur32        hash.Hash32
 }
 
 // Returns new HLL instance configured to use the final 6 bits to denote the register (64 register total)
 func NewHLL() HLL {
-  return NewHLLWithRegisterBits(6)
+	return NewHLLWithRegisterBits(6)
 }
 
 // Returns new HLL instance with the specified number of register bits
 func NewHLLWithRegisterBits(numRegisterBits int) HLL {
-  numRegisters := int(math.Exp2(float64(numRegisterBits)))
-  registers := make([]int, numRegisters)
-  murmur32 := murmur3.New32()
-  hllInstance := HLL {numRegisterBits, registers, murmur32}
-  return hllInstance
+	numRegisters := int(math.Exp2(float64(numRegisterBits)))
+	registers := make([]int, numRegisters)
+	murmur32 := murmur3.New32()
+	hllInstance := HLL{numRegisterBits, registers, murmur32}
+	return hllInstance
 }
 
 // Add string value to the HLL. MurmurHash is used to get 32 bit hash of string bytes.
 func (hll HLL) Add(value string) {
-  hll.murmur32.Write([]byte(value))
-  hashedValue := hll.murmur32.Sum32()
-  // bit mask to fetch bits representing register index to update
-  maskRegisterBits := ^uint32(0) >> uint32(32 - hll.numRegisterBits)
-  registerIndex := uint32(hashedValue & maskRegisterBits)
-  remainingBits := hashedValue >> uint32(hll.numRegisterBits)
-  trailingZeroes := bits.TrailingZeros32(remainingBits)
-  registerValue := 0
-  if trailingZeroes != 32 {
-    registerValue = trailingZeroes
-  }
-  hll.registers[registerIndex] = int(math.Max(float64(hll.registers[registerIndex]), float64(registerValue + 1)))
+	hll.murmur32.Write([]byte(value))
+	hashedValue := hll.murmur32.Sum32()
+	// bit mask to fetch bits representing register index to update
+	maskRegisterBits := ^uint32(0) >> uint32(32-hll.numRegisterBits)
+	registerIndex := uint32(hashedValue & maskRegisterBits)
+	remainingBits := hashedValue >> uint32(hll.numRegisterBits)
+	trailingZeroes := bits.TrailingZeros32(remainingBits)
+	registerValue := 0
+	if trailingZeroes != 32 {
+		registerValue = trailingZeroes
+	}
+	hll.registers[registerIndex] = int(math.Max(float64(hll.registers[registerIndex]), float64(registerValue+1)))
 }
 
 // Computes the count/cardinality from the instance's register values
 func (hll HLL) Count() float64 {
-  harmonicMean := 0.0
-  numZeroes := 0.0
-  for _, registerVal := range hll.registers {
-    harmonicMean += 1.0 / math.Pow(2.0, float64(registerVal))
-    if registerVal == 0 {
-      numZeroes += 1.0
-    }
-  }
-  harmonicMean = 1.0 / harmonicMean
-  // TODO: figure out what alpha param means
-  estimate := getAlphaByNumRegisters(len(hll.registers)) * math.Pow(float64(len(hll.registers)), float64(2)) * float64(harmonicMean)
-  count := 0.0
-  // small range correction
-  if estimate <= (5.0 / 2.0) * float64(len(hll.registers)) {
-    if numZeroes == 0 {
-      count = estimate
-    } else {
-      count = math.Round(float64(len(hll.registers)) * math.Log2(float64(len(hll.registers)) / numZeroes))
-    }
+	harmonicMean := 0.0
+	numZeroes := 0.0
+	for _, registerVal := range hll.registers {
+		harmonicMean += 1.0 / math.Pow(2.0, float64(registerVal))
+		if registerVal == 0 {
+			numZeroes += 1.0
+		}
+	}
+	harmonicMean = 1.0 / harmonicMean
+	// TODO: figure out what alpha param means
+	estimate := getAlphaByNumRegisters(len(hll.registers)) * math.Pow(float64(len(hll.registers)), float64(2)) * float64(harmonicMean)
+	count := 0.0
+	// small range correction
+	if estimate <= (5.0/2.0)*float64(len(hll.registers)) {
+		if numZeroes == 0 {
+			count = estimate
+		} else {
+			count = math.Round(float64(len(hll.registers)) * math.Log2(float64(len(hll.registers))/numZeroes))
+		}
 
-    return count
-  }
+		return count
+	}
 
-  if estimate <= 1.0 / 30.0 * math.Exp2(32.0) {
-    // intermediate range, no correction
-    count = estimate
-  } else {
-    // large range correction
-    count = math.Pow(-2.0, 32.0) * math.Log2(1 - estimate / math.Pow(2, 32))
-  }
+	if estimate <= 1.0/30.0*math.Exp2(32.0) {
+		// intermediate range, no correction
+		count = estimate
+	} else {
+		// large range correction
+		count = math.Pow(-2.0, 32.0) * math.Log2(1-estimate/math.Pow(2, 32))
+	}
 
-  return count
+	return count
 }
 
 // Merges two HLL instances by computing max(HLL1.register[i], HLL2.register[i]) for i in [0, numRegisters - 1]. Both
 // HLLs must have the same number of register bits.
-func (hll HLL) Merge(other HLL) (error) {
-  // verify that num register bits are equal
-  if hll.numRegisterBits != other.numRegisterBits {
-    return errors.New("hll: can't merge HLLs with different number of registers")
-  }
+func (hll HLL) Merge(other HLL) error {
+	// verify that num register bits are equal
+	if hll.numRegisterBits != other.numRegisterBits {
+		return errors.New("hll: can't merge HLLs with different number of registers")
+	}
 
-  for index, registerVal := range other.registers {
-    hll.registers[index] = int(math.Max(float64(registerVal), float64(hll.registers[index])))
-  }
+	for index, registerVal := range other.registers {
+		hll.registers[index] = int(math.Max(float64(registerVal), float64(hll.registers[index])))
+	}
 
-  return nil
+	return nil
 }
 
 func getAlphaByNumRegisters(numRegisters int) float64 {
-  var alpha float64
-  if numRegisters == 16 {
-    alpha = 0.673
-  } else if numRegisters == 32 {
-    alpha = 0.697
-  } else if numRegisters == 64 {
-    alpha = 0.709
-  } else {
-    alpha = 0.7213 / (1 + 1.079 / float64(numRegisters))
-  }
+	var alpha float64
+	if numRegisters == 16 {
+		alpha = 0.673
+	} else if numRegisters == 32 {
+		alpha = 0.697
+	} else if numRegisters == 64 {
+		alpha = 0.709
+	} else {
+		alpha = 0.7213 / (1 + 1.079/float64(numRegisters))
+	}
 
-  return alpha
+	return alpha
 }
--- hll_test.go
+++ hll_test.go
@@ -1,98 +1,98 @@
 package hll
 
 import (
-  "errors"
-  "testing"
-  "github.com/stretchr/testify/assert"
-  "github.com/golang/mock/gomock"
-  "go-hll/mocks"
+	"errors"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"go-hll/mocks"
+	"testing"
 )
 
 func TestNewHLLWithRegisterBits(t *testing.T) {
-  numRegisterBits := 6
-  numRegisters := 64
-  hllInstance := NewHLLWithRegisterBits(numRegisterBits)
-  assert.Equal(t, numRegisterBits, hllInstance.numRegisterBits, "Num register bits should be equal")
-  expectedRegisters := make([]int, numRegisters)
-  assert.Equal(t, expectedRegisters, hllInstance.registers, "Registers should be equal")
+	numRegisterBits := 6
+	numRegisters := 64
+	hllInstance := NewHLLWithRegisterBits(numRegisterBits)
+	assert.Equal(t, numRegisterBits, hllInstance.numRegisterBits, "Num register bits should be equal")
+	expectedRegisters := make([]int, numRegisters)
+	assert.Equal(t, expectedRegisters, hllInstance.registers, "Registers should be equal")
 }
 
 func TestAdd(t *testing.T) {
-  numRegisterBits := 6
-  numRegisters := 64
-  registers := make([]int, numRegisters)
-  mockCtrl := gomock.NewController(t)
-  defer mockCtrl.Finish()
-  mockMurmur32 := mocks.NewMockHash32(mockCtrl)
-  hllInstance := HLL {numRegisterBits, registers, mockMurmur32}
-  mockMurmur32.EXPECT().Write([]byte("hello"))
-  // 111000001010011100110110
-  mockMurmur32.EXPECT().Sum32().Return(uint32(3688933174))
-  hllInstance.Add("hello")
-  for idx, registerVal := range hllInstance.registers {
-    // last 6 bits: 110110 = 54
-    if idx == 54 {
-      assert.Equal(t, 3, registerVal)
-    } else {
-      assert.Equal(t, 0, registerVal)
-    }
-  }
+	numRegisterBits := 6
+	numRegisters := 64
+	registers := make([]int, numRegisters)
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockMurmur32 := mocks.NewMockHash32(mockCtrl)
+	hllInstance := HLL{numRegisterBits, registers, mockMurmur32}
+	mockMurmur32.EXPECT().Write([]byte("hello"))
+	// 111000001010011100110110
+	mockMurmur32.EXPECT().Sum32().Return(uint32(3688933174))
+	hllInstance.Add("hello")
+	for idx, registerVal := range hllInstance.registers {
+		// last 6 bits: 110110 = 54
+		if idx == 54 {
+			assert.Equal(t, 3, registerVal)
+		} else {
+			assert.Equal(t, 0, registerVal)
+		}
+	}
 }
 
 func TestCountSmallRangeCorrection(t *testing.T) {
-  numRegisterBits := 6
-  numRegisters := 64
-  registers := make([]int, numRegisters)
-  mockCtrl := gomock.NewController(t)
-  defer mockCtrl.Finish()
-  mockMurmur32 := mocks.NewMockHash32(mockCtrl)
-  hllInstance := HLL {numRegisterBits, registers, mockMurmur32}
-  mockMurmur32.EXPECT().Write([]byte("hello"))
-  // 111000001010011100110110
-  mockMurmur32.EXPECT().Sum32().Return(uint32(2903491477))
-  hllInstance.Add("hello")
-  assert.Equal(t, 1.0, hllInstance.Count())
+	numRegisterBits := 6
+	numRegisters := 64
+	registers := make([]int, numRegisters)
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockMurmur32 := mocks.NewMockHash32(mockCtrl)
+	hllInstance := HLL{numRegisterBits, registers, mockMurmur32}
+	mockMurmur32.EXPECT().Write([]byte("hello"))
+	// 111000001010011100110110
+	mockMurmur32.EXPECT().Sum32().Return(uint32(2903491477))
+	hllInstance.Add("hello")
+	assert.Equal(t, 1.0, hllInstance.Count())
 }
 
 func TestCountSmallRangeNoCorrection(t *testing.T) {
-  // TODO
+	// TODO
 }
 
 func TestCountIntermediateRangeNoCorrection(t *testing.T) {
-  // TODO
+	// TODO
 }
 
 func TestCountLargeRangeCorrection(t *testing.T) {
-  // TODO
+	// TODO
 }
 
 func TestMergeUnequalRegisterBits(t *testing.T) {
-  hllInstance1 := NewHLLWithRegisterBits(6)
-  hllInstance2 := NewHLLWithRegisterBits(5)
-  assert.Equal(t, errors.New("hll: can't merge HLLs with different number of registers"), hllInstance1.Merge(hllInstance2))
+	hllInstance1 := NewHLLWithRegisterBits(6)
+	hllInstance2 := NewHLLWithRegisterBits(5)
+	assert.Equal(t, errors.New("hll: can't merge HLLs with different number of registers"), hllInstance1.Merge(hllInstance2))
 }
 
 func TestMergeValid(t *testing.T) {
-  numRegisterBits := 6
-  hllInstance := NewHLLWithRegisterBits(numRegisterBits)
-  hllInstance.registers[10] = 5
-  other := NewHLLWithRegisterBits(numRegisterBits)
-  other.registers[5] = 2
-  assert.Nil(t, hllInstance.Merge(other))
-  for idx, registerVal := range hllInstance.registers {
-    if idx == 5 {
-      assert.Equal(t, 2, registerVal)
-    } else if idx == 10 {
-      assert.Equal(t, 5, registerVal)
-    } else {
-      assert.Equal(t, 0, registerVal)
-    }
-  }
+	numRegisterBits := 6
+	hllInstance := NewHLLWithRegisterBits(numRegisterBits)
+	hllInstance.registers[10] = 5
+	other := NewHLLWithRegisterBits(numRegisterBits)
+	other.registers[5] = 2
+	assert.Nil(t, hllInstance.Merge(other))
+	for idx, registerVal := range hllInstance.registers {
+		if idx == 5 {
+			assert.Equal(t, 2, registerVal)
+		} else if idx == 10 {
+			assert.Equal(t, 5, registerVal)
+		} else {
+			assert.Equal(t, 0, registerVal)
+		}
+	}
 }
 
 func TestGetAlphaByNumRegisters(t *testing.T) {
-  assert.Equal(t, 0.673,  getAlphaByNumRegisters(16))
-  assert.Equal(t, 0.697, getAlphaByNumRegisters(32))
-  assert.Equal(t, 0.709, getAlphaByNumRegisters(64))
-  assert.Equal(t, 0.7213 / (1 + 1.079 / 128.0), getAlphaByNumRegisters(128))
+	assert.Equal(t, 0.673, getAlphaByNumRegisters(16))
+	assert.Equal(t, 0.697, getAlphaByNumRegisters(32))
+	assert.Equal(t, 0.709, getAlphaByNumRegisters(64))
+	assert.Equal(t, 0.7213/(1+1.079/128.0), getAlphaByNumRegisters(128))
 }

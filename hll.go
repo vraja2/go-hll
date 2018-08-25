e89fd017f8ed0c1b78012b83c973341453905578
Vignesh Raja <vignesh@optimizely.com>
2018-08-24 23:44:13 -0700

    Flesh out range corrections for Count

--- hll.go
+++ hll.go
@@ -5,7 +5,7 @@ import (
   "hash"
   "math"
   "math/bits"
-  // "fmt"
+//  "fmt"
 )
 
 type HLL struct {
@@ -43,15 +43,54 @@ func (hll HLL) Add(value string) {
 
 func (hll HLL) Count() float64 {
   harmonicMean := 0.0
+  numZeroes := 0.0
   for _, registerVal := range hll.registers {
-    harmonicMean += math.Exp2(float64(-1 * registerVal))
+    harmonicMean += 1.0 / math.Pow(2.0, float64(registerVal))
+    if registerVal == 0 {
+      numZeroes += 1.0
+    }
   }
-  harmonicMean = math.Pow(harmonicMean, -1)
+  harmonicMean = 1.0 / harmonicMean
   // TODO: figure out what alpha param means
-  count := 0.709 * math.Pow(float64(len(hll.registers)), float64(2)) * float64(harmonicMean)
+  estimate := getAlphaByNumRegisters(len(hll.registers)) * math.Pow(float64(len(hll.registers)), float64(2)) * float64(harmonicMean)
+  count := 0.0
+  // small range correction
+  if estimate <= (5.0 / 2.0) * float64(len(hll.registers)) {
+    if numZeroes == 0 {
+      count = estimate
+    } else {
+      count = math.Round(float64(len(hll.registers)) * math.Log2(float64(len(hll.registers)) / numZeroes))
+    }
+
+    return count
+  }
+
+  if estimate <= 1.0 / 30.0 * math.Exp2(32.0) {
+    // intermediate range, no correction
+    count = estimate
+  } else {
+    // large range correction
+    count = math.Pow(-2.0, 32.0) * math.Log2(1 - estimate / math.Pow(2, 32))
+  }
+
   return count
 }
 
 func Merge() {
 
+}
+
+func getAlphaByNumRegisters(numRegisters int) float64 {
+  var alpha float64
+  if numRegisters == 16 {
+    alpha = 0.673
+  } else if numRegisters == 32 {
+    alpha = 0.697
+  } else if numRegisters == 64 {
+    alpha = 0.709
+  } else {
+    alpha = 0.7213 / (1 + 1.079 / float64(numRegisters))
+  }
+
+  return alpha
 }
